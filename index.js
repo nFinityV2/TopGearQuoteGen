739858d1503d22e3a40037bb75e0400efb85a705
nFinityV2 <99304246+nFinityV2@users.noreply.github.com>
2022-04-30 01:59:39 +1000

    Adding quotes

---
+++ index.js
@@ -0,0 +1,8 @@
+var quotes = [
+    "This is brilliant, but I like this. - Jeremy Clarkson",
+    "Top Gear. Ambitous, but rubbish. - Jeremy Clarkson",
+    "Speed and power solves many things. - Jeremy Clarkson",
+    "France is a country, you have to drive through to get to Italy. That all it's for. - James May",
+    "(The French) are a bunch of treacherous, land burning, work-shy peasants. - James May",
+    "Speed has never killed anyone. Suddenly becoming stationary, that's what gets you. - Jeremy Clarkson"
+];

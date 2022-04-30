6f43c3c2f4c12fa7dab04cbb2e5a3a6076e490a6
nFinityV2 <99304246+nFinityV2@users.noreply.github.com>
2022-04-30 16:28:24 +1000

    Added random selection

--- index.js
+++ index.js
@@ -4,5 +4,16 @@ var quotes = [
     "Speed and power solves many things. - Jeremy Clarkson",
     "France is a country, you have to drive through to get to Italy. That all it's for. - James May",
     "(The French) are a bunch of treacherous, land burning, work-shy peasants. - James May",
-    "Speed has never killed anyone. Suddenly becoming stationary, that's what gets you. - Jeremy Clarkson"
+    "Speed has never killed anyone. Suddenly becoming stationary, that's what gets you. - Jeremy Clarkson",
+    "Oh crickey! It's the rozzers. - James May",
+    "Well, how hard can it be? - Jeremy Clarkson",
+    "Sometimes my genius....... is almost frightening. - Jeremy Clarkson",
+    "Let's not get bogged down with...... - Jeremy Clarkson",
+    "POWEEEEEEEEEEEEEERRRRRRRRRRR - Jeremy Clarkson",
+    "HAMMOND, YOU IDIOT. YOU REVERSED INTO THE SPORTS LORRY - Jeremy Clarkson",
+    "He's saying, take the Third Reich. - James May"
 ];
+
+var arrRandom = Math.floor(Math.random() * quotes.length);
+var selectedQuote = quotes[arrRandom];
+document.write(selectedQuote);
